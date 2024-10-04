a7463c2468b1b1d19e33007f00b3da2aad66342f
Rogério Santos <rogerio.dosantos@ifpr.edu.br>
2024-10-04 14:30:01 -0300

    UpDate

--- src/predict.py
+++ src/predict.py
@@ -2,11 +2,10 @@ import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from neuralforecast.core import NeuralForecast
 from neuralforecast.auto import AutoTFT
 from neuralforecast.losses.pytorch import MAE
 from ray import tune
-
+from neuralforecast.core import NeuralForecast
 from statsmodels.tools.eval_measures import rmse  # root mean squared error
 from statsmodels.tools.eval_measures import rmspe  # root mean squared percentage error
 from statsmodels.tools.eval_measures import maxabs  # maximum absolute error
@@ -18,8 +17,8 @@ from statsmodels.tools.eval_measures import iqr  # interquartile range of error
 import math
 
 
-def predict_with_loaded_model(data_path, model_path, full_horizon, config):
-    """Carrega um modelo treinado e faz previsões."""
+def train_and_predict(data_path, horizon, config, output_path, full_horizon):
+    """Treina o modelo, salva e faz previsões."""
 
     # Load the data
     Y_df = pd.read_csv(data_path, sep=';')
@@ -29,26 +28,44 @@ def predict_with_loaded_model(data_path, model_path, full_horizon, config):
     Y_df = Y_df[0:max[-1][-1]]
 
     # Plota os dados originais
-     plt.figure(figsize=(6, 3))
-     plt.plot(Y_df.y, 'k')
-     plt.axis([0, len(list(Y_df.y)), 0, 100])
-     plt.xlabel('Time (h)')
-     plt.ylabel('Level (%)')
-     plt.grid(linestyle='-', which='both')
-     plt.show()
+    plt.figure(figsize=(6, 3))
+    plt.plot(Y_df.y, 'k')
+    plt.axis([0, len(list(Y_df.y)), 0, 100])
+    plt.xlabel('Time (h)')
+    plt.ylabel('Level (%)')
+    plt.grid(linestyle='-', which='both')
+    plt.show()
 
     Y_df['ds'] = pd.to_datetime(Y_df['ds'], format='%d/%m/%y %H:%M')
 
-    # Carrega o modelo treinado
+    # Treinamento do modelo
+    start_time = time.time()
+    models = [AutoTFT(h=horizon,
+                      loss=MAE(),
+                      config=config,
+                      num_samples=10)]
+
+    nf = NeuralForecast(
+        models=models,
+        freq='H')
+
+    nf.fit(df=Y_df)
+    nf.save(path=output_path,
+            model_index=None,
+            overwrite=True,
+            save_dataset=True)
+    end_time = time.time()
+    print('Time to train model:', end_time - start_time)
+
+    # Carregando o modelo treinado
     model = AutoTFT(h=1, loss=MAE(), config=config, num_samples=10)
     nf_loaded = NeuralForecast(models=[model], freq='H')
-    nf_loaded = NeuralForecast.load(path=model_path)
+    nf_loaded = NeuralForecast.load(path=output_path)
 
     # Faz previsões
     Y_hat_df = nf_loaded.predict(df=Y_df)
-
     y_pred = Y_hat_df.AutoTFT
-    y_true = Y_df[-1:].y
+    y_true = Y_df[-horizon:].y
 
     end_time = time.time()
     print('Time to create models:', end_time - start_time)
@@ -58,7 +75,7 @@ def predict_with_loaded_model(data_path, model_path, full_horizon, config):
         f'{(rmse(y_true, y_pred)):.2E} & {(rmspe(y_true, y_pred)):.2E} & {(maxabs(y_true, y_pred)):.2E} & {(meanabs(y_true, y_pred)):.2E} & {(medianabs(y_true, y_pred)):.2E} '
     )
 
-    full_horizon = 20
+    # Previsões de múltiplos passos
     n_predicts = math.ceil(full_horizon / model.h)
     combined_train = Y_df
     forecasts = []
@@ -74,10 +91,10 @@ def predict_with_loaded_model(data_path, model_path, full_horizon, config):
 
 if __name__ == '__main__':
     data_path = '../data/data.csv'
-    model_path = 'checkpoints/test_run/'
+    horizon = 1
     full_horizon = 20
     config = {
-        "input_size": tune.choice([1]),
+        "input_size": tune.choice([horizon]),
         "hidden_size": tune.choice([8, 32]),
         "n_head": tune.choice([2, 8]),
         "learning_rate": tune.loguniform(1e-4, 1e-1),
@@ -87,4 +104,5 @@ if __name__ == '__main__':
         "check_val_every_n_epoch": tune.choice([100]),
         "random_seed": tune.randint(1, 20),
     }
-    predict_with_loaded_model(data_path, model_path, full_horizon, config)
+    output_path = 'checkpoints/test_run/'
+    train_and_predict(data_path, horizon, config, output_path, full_horizon)
